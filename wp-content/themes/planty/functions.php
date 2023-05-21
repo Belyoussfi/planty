fa258d874ee85d01feacf25729fb333b0102e64c
Belyoussfi <elyoussfi.boucheragmail.com>
2023-05-21 12:24:55 +0200

    Header: Hook crée avec bouton Admin

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -5,3 +5,24 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
+// Hook pour ajouter Admin si connecté 
+function my_wp_nav_menu_args (
+    $args ='' ) {
+        if (is_user_logged_in() )
+            {
+                $args['menu'] = 6 ;
+            } else {
+                $args['menu'] = 5;
+            }
+            return $args;    
+    }
+    add_filter(
+        'wp_nav_menu_args',
+        'my_wp_nav_menu_args'
+    );  
+   
+
+   
+        
+    
